086f0fa2a7fea82a59392d740a326e64474468a5
newython <newython@gmail.com>
2020-11-29 13:25:56 -0400

    API Rest consulta cidades do Brasil

---
+++ src/test/java/com/github/newython/citiesapi/CitiesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.newython.citiesapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CitiesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
